50b13549207efb3b5d8235e2e332382b463b460b
hieuhoi91 <hieuhoi912002@uhoi912002gmail.com>
2024-03-27 00:54:46 +0700

    feat: editor

--- src/view/Admin/index.tsx
+++ src/view/Admin/index.tsx
@@ -8,6 +8,7 @@ import '@blocknote/react/style.css';
 import { BlogApi } from '@/api/blog-api';
 
 // Uploads a file to tmpfiles.org and returns the URL to the uploaded file.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 async function uploadFile(file: any) {
   const files = await BlogApi.uploadFiles(file);
 
